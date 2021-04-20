7017572366d938b88f13edcab5f0e916dd816d09
NicolaC <nicolacrawshaw@developersinstitute.co.nz>
2021-04-21 09:06:53 +1200

    create todos.js file with routes

---
+++ src/services/todos.js
@@ -0,0 +1,47 @@
+const pool = require("../db");
+
+const formatTodo = (todo) => {
+  const newTodo = todo;
+  newTodo.id = todo.todo_id;
+  delete newTodo.todo_id;
+  return newTodo;
+};
+
+const createTodo = async (description) => {
+  const newTodo = await pool.query(
+    "INSERT INTO todo (description) VALUES($1) RETURNING *",
+    [description]
+  );
+  return formatTodo(newTodo.rows[0]);
+};
+
+const getTodos = async () => {
+  const todos = await pool.query("SELECT * FROM todo");
+  return todos.rows.map(formatTodo);
+};
+
+const getTodo = async (id) => {
+  const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+  return formatTodo(todo.rows[0]);
+};
+
+const updateTodo = async (id, description) => {
+  await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [
+    description,
+    id,
+  ]);
+  return "Todo was updated!";
+};
+
+const deleteTodo = async (id) => {
+  await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+  return "Todo was deleted!";
+};
+
+module.exports = {
+  getTodos,
+  getTodo,
+  createTodo,
+  deleteTodo,
+  updateTodo,
+};
